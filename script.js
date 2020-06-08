b627301f75555278427dfd42adea040ec02c1159
Aurélien Lheureux <dev@aurelienlheureux.com>
2020-06-08 19:53:56 +0200

    Montage page panier

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * Instruction popin :
+ * Vous devez insérer ce script dans la page panier.html
+ * Pour utiliser le script de popin vous devez mettre l'id "buttonReduction" sur le boutton "Afficher le code promo" et l'id popinReduction sur  
+ * Pour fermer la popin il faudra créer un bouton et lui attribuer l'id "closePopinReduction"
+ */
+(function(){
+	window.addEventListener('load',function(e){
+		var popin = document.getElementById('popinReduction');
+		var button = document.getElementById('buttonReduction');
+		var close = document.getElementById('closePopinReduction');
+
+		popin.style.display="none";
+
+		button.addEventListener('click',function(){
+			popin.style.display = "block";
+		});
+		close.addEventListener('click',function(){
+			popin.style.display = "none";
+		})
+	})
+})();
